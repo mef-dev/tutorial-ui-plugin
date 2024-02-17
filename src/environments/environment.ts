8d86599eed7a04083c5718e70c0db2997659825a
Marija Shahrai <Marija.Shahrai@mef.dev>
2024-02-17 12:22:45 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,5 @@ import { PLUGIN_VERSION } from './version'
 export const environment = {
   production: false,
   version: PLUGIN_VERSION.version,
-  bauth: 'Bauth1302:Bauth1302',
-  apiUrl: `https://api.mef.dev`
+  bauth: 'NAME:PASS'
 }
